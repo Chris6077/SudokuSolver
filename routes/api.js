e1b748e5f92deb7e3d9658307c16f72781e50fc3
Glitch (sudokusolver-cv) <none>
2022-04-08 13:05:55 +0000

    finish

--- routes/api.js
+++ routes/api.js
@@ -13,27 +13,29 @@ module.exports = function (app) {
       let value = req.body.value;
     
       if(puzzle && coordinate && value){
-        if(/^[1-9]{1}$/.test(value)){
+        if(coordinate.length != 2)
+          res.send({ error: 'Invalid coordinate' });
+        else if(/^[1-9]{1}$/.test(value)){
           let validationResult = solver.validate(puzzle);
           if(validationResult)
             res.send(validationResult);
           else{
             let rowIdx = coordinate.toUpperCase().charCodeAt(0) - 65;
             let columnIdx = coordinate[1] - 1;
-            
+                        
             if(rowIdx >= 0 && rowIdx <= 8 && columnIdx >= 0 && columnIdx <= 8){
               let conflict = [];
-              if(solver.checkRowPlacement(puzzle, rowIdx, columnIdx, value))
-                conflict.push("row");
-              if(solver.checkColPlacement(puzzle, rowIdx, columnIdx, value))
-                conflict.push("column");
-              if(solver.checkRegionPlacement(puzzle, rowIdx, columnIdx, value))
-                conflict.push("region");
+              if(!solver.checkRowPlacement(puzzle, rowIdx, columnIdx, value))
+                conflict.push('row');
+              if(!solver.checkColPlacement(puzzle, rowIdx, columnIdx, value))
+                conflict.push('column');
+              if(!solver.checkRegionPlacement(puzzle, rowIdx, columnIdx, value))
+                conflict.push('region');
                             
               res.send({ valid: conflict.length == 0, conflict });
             }
             else
-              res.send({ "error": "Invalid coordinate" });
+              res.send({ error: 'Invalid coordinate' });
           }
         }
         else
@@ -54,10 +56,13 @@ module.exports = function (app) {
           res.send(validationResult);
         else if(!validationResult){
           let solved = solver.solve(puzzle);
-          res.send({ solution: solved });
+          if(solved)
+            res.send({ solution: solved });
+          else
+            res.send({ error: 'Puzzle cannot be solved'});
         }
       }
       else
-        res.send({ "error": 'Required field missing' });
+        res.send({ error: 'Required field missing' });
     });
 };
--- server.js
+++ server.js
@@ -37,20 +37,22 @@ app.use(function(req, res, next) {
 
 //Start our server and tests!
 const PORT = process.env.PORT || 3000
-app.listen(PORT, function () {
-  console.log("Listening on port " + PORT);
-  // process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (error) {
-        console.log('Tests are not valid:');
-        console.error(error);
-      }
-    }, 1500);
-  }
-});
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.log("Listening on port " + PORT);
+    // process.env.NODE_ENV='test'
+    if (process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch (error) {
+          console.log('Tests are not valid:');
+          console.error(error);
+        }
+      }, 1500);
+    }
+  });
+}
 
 module.exports = app; // for testing
